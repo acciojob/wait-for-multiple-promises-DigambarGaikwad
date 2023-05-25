ef9f1366d8b7ad22f4ec20316577df9bfa379190
DigambarGaikwad <104820408+DigambarGaikwad@users.noreply.github.com>
2023-05-25 11:47:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const promises = [];
+
+for (let i = 0; i < 3; i++) {
+  promises.push(new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(i + 1);
+    }, Math.floor(Math.random() * 3) + 1);
+  }));
+}
+
+Promise.all(promises).then((results) => {
+  const table = document.querySelector('#output');
+
+  // Remove the loading text
+  const loadingRow = table.querySelector('.loading');
+  loadingRow.remove();
+
+  // Populate the table with the results
+  results.forEach((result, index) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>Promise ${index + 1}</td>
+      <td>${result}</td>
+    `;
+    table.appendChild(row);
+  });
+
+  // Calculate the total time taken
+  const totalTime = results.reduce((acc, result) => acc + result, 0);
+
+  // Add a row for the total time
+  const totalRow = document.createElement('tr');
+  totalRow.innerHTML = `
+    <td>Total</td>
+    <td>${totalTime}</td>
+  `;
+  table.appendChild(totalRow);
+});
